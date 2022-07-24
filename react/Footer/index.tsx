28006578dc84c153363475e056926fa036e063ed
cuvar <luca-mueller@gmx.net>
2022-07-24 17:23:08 +0200

    react: navbar, footer, wrapper, link

---
+++ react/Footer/index.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="flex justify-center px-5 py-5 bg-darkred-400">
+      <div>Footer</div>
+    </div>
+  );
+}
---
+++ react/Link/index.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+interface IProps {
+  href: string;
+  title: string;
+  tab?: "new" | "self";
+}
+
+export default function CustomLink(props: IProps) {
+  return (
+    <Link href={props.href}>
+      {props.tab === "new" ? (
+        <a className="hover:underline" target="_blank" rel="noopener">
+          {props.title}
+        </a>
+      ) : (
+        <a className="hover:underline">{props.title}</a>
+      )}
+    </Link>
+  );
+}
