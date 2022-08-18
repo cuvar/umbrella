1ace6b3c2a567b75505414a7c014c5be6b6672f2
cuvar <luca-mueller@gmx.net>
2022-08-18 20:59:17 +0200

    feat: add export for react

---
+++ react/index.ts
@@ -0,0 +1,15 @@
+import Footer from "./Footer/index";
+import Image from "./Image/index";
+import Link from "./Link/index";
+import Navbar from "./Navbar/index";
+import SiteWrapper from "./SiteWrapper/index";
+import Heading from "./Text/heading";
+
+export default {
+  Footer,
+  Image,
+  Link,
+  Navbar,
+  SiteWrapper,
+  Heading,
+};
